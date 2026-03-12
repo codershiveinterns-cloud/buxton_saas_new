da96f88dfc4c961a874f9882d7e30f0dbfc3ff16
kanishsingh510 <SINGHKANISHOCT@GMAIL.COM>
2026-03-12 12:48:26 +0530

    update api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Backend server port
+    baseURL: 'https://buxton-saas-new.onrender.com/api', // Backend server port
     headers: {
         'Content-Type': 'application/json',
     },
